386677f9a9b25d93bb93a0aa492e18b9d63fcf7e
James Becker <48602730+jameskbecker@users.noreply.github.com>
2023-06-20 14:42:17 +0100

    Update hero-info.tsx

--- components/hero-info.tsx
+++ components/hero-info.tsx
@@ -37,8 +37,8 @@ export const HeroInfo = () => {
         className="container flex flex-col items-center justify-start gap-8 overflow-hidden"
       >
         <p className="max-w-[700px] text-lg text-muted-foreground">
-          Hey, my name is James! I’ve been having an awesome experience working
-          with web technologies for the past 5 years.
+          Hey, my name is James. I’ve been having an awesome experience working
+          with web technologies for the past 5 years!
         </p>
         <a
           href={siteConfig.links.heroCta}
