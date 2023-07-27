9b061c5da9e756b74a168c27c1ca76177d5e11c1
James Becker <48602730+jameskbecker@users.noreply.github.com>
2023-07-27 10:50:02 +0200

    Fix spelling

--- content/about-me.ts
+++ content/about-me.ts
@@ -2,7 +2,7 @@ const aboutMe = `I began my web development journey **five years** ago, teaching
 
 By the end of my first year, I found myself working on my very first paid project, which eventually led to a job offer. However, I made the decision to continue freelancing, as it allowed me to strike a **balance** between work and my university studies, where I could truly **master my craft**.
 
-During my final year at university, I decided to **specialise in frontend engineering**. Recently, I had the privilege of serving as a frontend engineer for an early-stage e-commerce start-up. I worked closely with a small, **cross-functional** team on a enterprise-level codebase. My key responsiblities included establishing a **scaleable** frontend, contributing my ideas to product design and staying in **close communcation** with my team.
+During my final year at university, I decided to **specialise in frontend engineering**. Recently, I had the privilege of serving as a frontend engineer for an early-stage e-commerce start-up. I worked closely with a small, **cross-functional** team on a enterprise-level codebase. My key responsibilities included establishing a **scaleable** frontend, contributing my ideas to product design and staying in **close communication** with my team.
 
 My strengths lie in **TypeScript, React and related frameworks like NextJS**. I also have solid foundations in the more traditional HTML, CSS and JavaScript which I used before transition to TypeScript and React.`;
 
